15f9f0ff7b4e151d121efd11f53ab1a843d4ea42
TheTangerineOwl <salmontangerine@mail.ru>
2025-08-02 13:29:52 +0700

    03: добавлено решение на основе 2-1 прошлого модуля.

---
+++ 03/src/contactsFile.c
@@ -0,0 +1,68 @@
+#include "contacts.h"
+
+ssize_t readContacts(const char* filename)
+{
+    int file = open(filename,
+        O_RDONLY | O_CREAT,
+        S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (file == -1)
+    {
+        perror("readContact");
+        return -1;
+    }
+
+    ssize_t count = 1;
+
+    Contact_t buffer;
+    ssize_t readBytes;
+    readBytes = read(file, &buffer, sizeof(buffer));
+    while (count <= MAX_CONTACTS_COUNT)
+    {
+        if (readBytes == -1)
+        {
+            perror("readContacts");
+            return -1;
+        }
+        if (!readBytes)
+            break;
+        contacts[count - 1] = buffer;
+        count++;
+        readBytes = read(file, &buffer, sizeof(buffer));
+    }
+
+    contactsCount = count - 1;
+    return contactsCount;
+}
+
+ssize_t writeContacts(const char* filename)
+{
+    int file = open(filename,
+        O_WRONLY | O_CREAT | O_TRUNC,
+        S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (file == -1)
+    {
+        perror("writeContact");
+        return -1;
+    }
+
+    if (!contactsCount)
+        return 0;
+
+    size_t count = 0;
+
+    Contact_t buffer;
+    ssize_t writeBytes = 0;
+    while (count < contactsCount)
+    {
+        if (writeBytes == -1)
+        {
+            perror("writeContacts");
+            return -1;
+        }
+        buffer = contacts[count];
+        count++;
+        writeBytes = write(file, &buffer, sizeof(buffer));
+    }
+
+    return count;
+}
